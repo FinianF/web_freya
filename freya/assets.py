4f267ac8c0d8691165f3a89f52e94dea60b8209f
root <loyaloyalika80@gmail.com>
2018-05-30 19:44:23 +0300

    че-та для popover

--- freya/assets.py
+++ freya/assets.py
@@ -2,8 +2,8 @@ from flask_assets import Bundle
 from freya import assets
 
 page_css = Bundle("css/page.css")
-map_css = Bundle("OpenLayers/ol.css", "css/map.css", filters="cssmin")
-map_js = Bundle("OpenLayers/ol.js", "js/map.js", filters="jsmin")
+map_css = Bundle("OpenLayers/ol.css", "css/map.css", "css/bootstrap.css", filters="cssmin")
+map_js = Bundle("OpenLayers/ol.js", "js/map.js", "js/bootstrap.js", "js/jquery-3.3.1.js", filters="jsmin")
 
 assets.register("page_css", page_css)
 assets.register("map_css", map_css)
