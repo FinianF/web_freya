847557f036fe092c33f45671502539ea360f3b63
FinianF <loyaloyalika80@gmail.com>
2018-06-09 16:21:55 +0300

    add popover

--- freya/static/js/map.js
+++ freya/static/js/map.js
@@ -5,14 +5,33 @@ var mousePositionControl = new ol.control.MousePosition({
     }
 });
 
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var overlay = new ol.Overlay({
+element: container,
+autoPan: true,
+autoPanAnimation: {
+  duration: 250
+}
+});
+
+closer.onclick = function() {
+overlay.setPosition(undefined);
+closer.blur();
+return false;
+};
+
 var map = new ol.Map({
     controls: ol.control.defaults().extend([
         mousePositionControl,
         new ol.control.ZoomSlider(),
         new ol.control.ScaleLine()
     ]),
+    overlays: [overlay],
     target: 'map'
-}); //норм почитай док-цию, ты заебала уже
+});
 
 var arcgisImagery = new ol.layer.Tile({
     source: new ol.source.TileArcGISRest({
@@ -26,3 +45,12 @@ var view = new ol.View({
     zoom: 3
 });
 map.setView(view);
+
+map.on('singleclick', function(evt) {
+var coordinate = evt.coordinate;
+var hdms = ol.coordinate.format(ol.proj.transform(
+    coordinate, 'EPSG:3857', 'EPSG:4326'), '{y}, {x}', 5);
+
+content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+overlay.setPosition(coordinate);
+});
