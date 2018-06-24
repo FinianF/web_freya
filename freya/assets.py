249f415a9ff004adef80baba2448b3a930c074e3
FinianF <loyaloyalika80@gmail.com>
2018-06-25 00:13:09 +0300

    DeSiGn

--- freya/assets.py
+++ freya/assets.py
@@ -5,9 +5,11 @@ page_css = Bundle("css/page.css")
 map_css = Bundle("OpenLayers/ol.css", "Bootstrap/bootstrap.css", "css/map.css",  filters="cssmin")
 bt_js = Bundle( "Bootstrap/jquery-3.3.1.js", "Bootstrap/popper.js", "Bootstrap/bootstrap.js", filters="jsmin")
 map_js = Bundle( bt_js, "OpenLayers/ol.js", "js/map.js",  filters="jsmin")
+cat_pic = Bundle("pic/kitty.jpg")
 
 assets.register("page_css", page_css)
 assets.register("map_css", map_css)
 assets.register("map_js", map_js)
+assets.register("cat_pic", cat_pic)
 
 
