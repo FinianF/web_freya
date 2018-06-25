1ad8eb7e006323e91471f73fcdb992570f115751
FinianF <loyaloyalika80@gmail.com>
2018-06-25 19:09:21 +0300

    Добавлен раздел <О нас> & Пофикшены мелкие баги

--- freya/__init__.py
+++ freya/__init__.py
@@ -10,23 +10,29 @@ template_folder="templates"
 )
 
 from config import *
+
 app.config.from_object(DevelopmentConfig())
 
 jsonrpc = JSONRPC(service_url='/api')
 db = SQLAlchemy()
 
-
 assets = Environment(app)
+
 import freya.assets
 
+mp_bp = Blueprint('main_page', __name__, url_prefix="/")
+ft_bp = Blueprint('freya_team', __name__, url_prefix="/freya_team")
 map_bp = Blueprint('map', __name__, url_prefix="/map")
 
+import freya.blueprints.page_render
 import freya.blueprints.map
 import freya.blueprints.rpc
 
+app.register_blueprint(mp_bp)
+app.register_blueprint(ft_bp)
 app.register_blueprint(map_bp)
-jsonrpc.init_app(app)
 
+jsonrpc.init_app(app)
 db.init_app(app)
 
 import freya.models
--- freya/assets.py
+++ freya/assets.py
@@ -1,14 +1,19 @@
 from flask_assets import Bundle
 from freya import assets
 
-page_css = Bundle("css/page.css")
-map_css = Bundle("OpenLayers/ol.css", "Bootstrap/bootstrap.css", "css/map.css",  filters="cssmin")
+page_css = Bundle("css/page.css", "css/freya_team.css")
+ft_css = Bundle("css/freya_team.css")
+map_css = Bundle("OpenLayers/ol.css", "css/map.css", filters="cssmin")
+bt_css = Bundle("Bootstrap/bootstrap.css", filters="cssmin")
 bt_js = Bundle( "Bootstrap/jquery-3.3.1.js", "Bootstrap/popper.js", "Bootstrap/bootstrap.js", filters="jsmin")
-map_js = Bundle( bt_js, "OpenLayers/ol.js", "js/map.js",  filters="jsmin")
+map_js = Bundle("OpenLayers/ol.js", "js/map.js", filters="jsmin")
 cat_pic = Bundle("pic/kitty.jpg")
 
+assets.register("bt_css", bt_css)
+assets.register("ft_css", ft_css)
 assets.register("page_css", page_css)
 assets.register("map_css", map_css)
+assets.register("bt_js", bt_js)
 assets.register("map_js", map_js)
 assets.register("cat_pic", cat_pic)
 
