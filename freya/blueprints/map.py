da235ed61120782613b5456f3d0b721115a1f19a
FinianF <loyaloyalika80@gmail.com>
2018-06-27 17:25:37 +0300

    Исправлена? ошибка при непустой БД

--- freya/blueprints/map.py
+++ freya/blueprints/map.py
@@ -20,17 +20,17 @@ def map_index():
         cdm = freya_packet.cdm_conc
         mq7 = freya_packet.mq7_conc
         geiger = freya_packet.geiger_ticks
+
+        try:
+            return render_template('map.html', lat=lat, lon=lon, desc=format_data)
+        except TemplateNotFound:
+            abort(404)
     except:
         return render_template('map.html', lat=0, lon=0, desc='Ого, похоже, что данных нет')
 
         "Концентрация CO2: {4} ppm<br>Концентрация CO: {5} ppm<br>Уровень радиации: {6} мкР/ч".format(
             lat, lon, press, temp, cdm, mq7, geiger
         )
-    
-    try:
-        return render_template('map.html', lat=lat, lon=lon, desc=format_data)
-    except TemplateNotFound:
-         abort(404)
 
 @map_bp.route('/map_data')
 def get_data():
@@ -45,6 +45,24 @@ def get_data():
         cdm = freya_packet.cdm_conc
         mq7 = freya_packet.mq7_conc
         geiger = freya_packet.geiger_ticks
+
+        format_data = "Координаты: {0}, {1}<br>Давление: {2} мм рт. ст.<br>Температура: {3} °C<br>" \
+                      "Концентрация CO2: {4} ppm<br>Концентрация CO: {5} ppm<br>Уровень радиации: {6} мкР/ч".format(
+            lat, lon, press, temp, cdm, mq7, geiger
+        )
+
+        telemetry = {
+            'lat': freya_packet.latitude,
+            'lon': freya_packet.longitude,
+            'press': round(freya_packet.bmp_press, 2),
+            'temp': round(freya_packet.bmp_temp, 2),
+            'cdm': freya_packet.cdm_conc,
+            'mq7': freya_packet.mq7_conc,
+            'geiger': freya_packet.geiger_ticks,
+            'format_data': format_data
+        }
+
+        return jsonify(telemetry)
     except:
         telemetry = {
             'lat': 0,
@@ -53,21 +71,3 @@ def get_data():
         }
 
         return jsonify(telemetry)
-
-    format_data = "Координаты: {0}, {1}<br>Давление: {2} мм рт. ст.<br>Температура: {3} °C<br>" \
-        "Концентрация CO2: {4} ppm<br>Концентрация CO: {5} ppm<br>Уровень радиации: {6} мкР/ч".format(
-            lat, lon, press, temp, cdm, mq7, geiger
-        )
-
-    telemetry = {
-        'lat' : freya_packet.latitude,
-        'lon' : freya_packet.longitude,
-        'press' : round(freya_packet.bmp_press, 2),
-        'temp' : round(freya_packet.bmp_temp, 2),
-        'cdm' : freya_packet.cdm_conc,
-        'mq7' : freya_packet.mq7_conc,
-        'geiger' : freya_packet.geiger_ticks,
-        'format_data' : format_data
-    }
-
-    return jsonify(telemetry)
